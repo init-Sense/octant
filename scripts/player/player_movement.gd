e2a6a8760f6a20c0c767131291f9e4dcd1611d64
bottolo <64470114+bottolo@users.noreply.github.com>
2024-07-26 15:36:51 +0200

    feature: Made player movement smoother
    Signed-off-by: Samuele Castiglia <samuele.castiglia@gntechonomy.co>

--- scripts/player/player_movement.gd
+++ scripts/player/player_movement.gd
@@ -1,7 +1,7 @@
 extends Node3D
 class_name PlayerMovement3D
 
-@export var speed : float = 5.0
+@export var speed : float = 3.0
 @export var jump_strength : float = 4.5
 @export var gravity : float = 9.8
 
@@ -9,7 +9,7 @@ var velocity : Vector3 = Vector3.ZERO
 var is_moving : bool = false
 
 @onready var player_body: CharacterBody3D = get_parent().get_node("PlayerBody3D")
-@onready var camera: PlayerCamera3D = get_parent().get_node("PlayerCamera3D")
+@onready var camera: PlayerCamera3D = get_parent().get_node("Head/PlayerCamera3D")
 
 func _ready():
 	print("PlayerMovement3D ready. player_body: ", player_body, ", camera: ", camera)
@@ -28,13 +28,19 @@ func _input(event):
 func handle_movement(delta):
 	var input_dir = Vector3.ZERO
 	if is_moving:
-		var camera_basis = camera.global_transform.basis
-		input_dir = -camera_basis.z.normalized()
-		input_dir = input_dir.rotated(Vector3.UP, player_body.rotation.y)
-		input_dir.y = 0
-	
-	velocity.x = move_toward(velocity.x, input_dir.x * speed, speed * delta)
-	velocity.z = move_toward(velocity.z, input_dir.z * speed, speed * delta)
+		var camera_transform = camera.global_transform
+
+		var forward = -camera_transform.basis.z
+		var right = camera_transform.basis.x
+
+		forward = Vector3(forward.x, 0, forward.z).normalized()
+		right = Vector3(right.x, 0, right.z).normalized()
+
+		input_dir = forward
+
+	var target_velocity = input_dir * speed
+	velocity.x = move_toward(velocity.x, target_velocity.x, speed * delta)
+	velocity.z = move_toward(velocity.z, target_velocity.z, speed * delta)
 
 func apply_gravity(delta):
 	if not player_body.is_on_floor():
