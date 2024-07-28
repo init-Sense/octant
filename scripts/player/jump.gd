be891d95cf6b625ccea2f7267cc8875c4476d4f8
Alessio De Nicolò <alessio1.denicolo@mail.polimi.it>
2024-07-28 13:33:33 +0200

    refactor: small jump script improvement

--- scripts/player/jump.gd
+++ scripts/player/jump.gd
@@ -4,7 +4,7 @@ class_name PlayerJump3D
 
 @onready var player: CharacterBody3D = $"../.."
 
-const JUMPING_SPEED: float = 8.0
+const JUMPING_SPEED: float = 10.0
 
 var has_jumped: bool = false
 var jump_velocity: Vector3
@@ -12,20 +12,20 @@ var jump_velocity: Vector3
 
 #region LIFECYCLE
 func _physics_process(_delta: float) -> void:
-	if player.is_jumping():
-		jump()
+		handle_jump()
 #endregion
 
 
 #region JUMP
-func jump() -> void:
-	has_jumped = true
-	player.velocity += Vector3(0.0, JUMPING_SPEED, 0.0)
-	player.move_and_slide()
-	
-	await get_tree().create_timer(0.1).timeout
-	
-	if has_jumped and player.is_on_floor():
-		has_jumped = false
-		player.set_action_grounded()
+func handle_jump() -> void:
+	if player.is_jumping():
+		has_jumped = true
+		player.velocity += Vector3(0.0, JUMPING_SPEED, 0.0)
+		player.move_and_slide()
+		
+		await get_tree().create_timer(0.1).timeout
+		
+		if has_jumped and player.is_on_floor():
+			has_jumped = false
+			player.set_action_grounded()
 #endregion
