8c372871efaa8b7894b7f30b72b0c5e9dd73b2da
bottolo <samuele.castiglia.98@gmail.com>
2024-07-29 12:14:05 +0200

    refactor: Crouch input moved to the input node

--- scripts/player/crouch.gd
+++ scripts/player/crouch.gd
@@ -30,46 +30,31 @@ var initial_head_position = Vector3.ZERO
 func _ready():
 	if not player or not collision or not head or not camera:
 		push_error("Required nodes not found in PlayerCrouch3D. Check the node structure.")
-
 	initial_player_height = collision.shape.height
 	initial_head_position = head.position
 
 
-func _input(event):
-	if Input.is_action_pressed('debug_crouch_up'):
-		crouch_up()
-	elif Input.is_action_pressed('debug_crouch_down'):
-		crouch_down()
-
-	if event is InputEventMouseButton:
-		if event.button_index ==  MOUSE_BUTTON_WHEEL_UP:
-			crouch_up()
-		elif event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
-			crouch_down()
-		handle_movement_speed()
-
-
 func _physics_process(delta):
 	if current_crouch_step != target_crouch_step:
-		current_crouch_step = move_toward(current_crouch_step, target_crouch_step, CROUCH_SPEED * CROUCH_STEPS * delta )
+		current_crouch_step = move_toward(current_crouch_step, target_crouch_step, CROUCH_SPEED * CROUCH_STEPS * delta)
 		update_player_height()
 #endregion
 
 
 #region CROUCHING
-func crouch_down():
+func down():
 	target_crouch_step = min(target_crouch_step + 1, CROUCH_STEPS)
+	handle_movement_speed()
 	print('Crouching down, target step:', target_crouch_step)
 
-
-func crouch_up():
+func up():
 	target_crouch_step = max(target_crouch_step - 1, 0)
+	handle_movement_speed()
 	print('Standing up, target step:', target_crouch_step)
 
 
 func handle_movement_speed() -> void:
 	movement.current_speed = movement.WALKING_SPEED * (1 - current_crouch_step / CROUCH_STEPS)
-	#print("Current speed: ", movement.current_speed)
 #endregion
 
 
@@ -78,9 +63,7 @@ func update_player_height():
 	var crouch_progress = float(current_crouch_step) / CROUCH_STEPS
 	var height_change = initial_player_height * CROUCH_AMOUNT * crouch_progress
 	var new_height = initial_player_height - height_change
-
 	collision.shape.height = new_height
 	head.position.y = initial_head_position.y - height_change
-
 	player.move_and_slide()
 #endregion
--- scripts/player/inputs.gd
+++ scripts/player/inputs.gd
@@ -1,9 +1,15 @@
 extends Node
 class_name PlayerInputsMouse
 
+
+#region NODES
+@onready var player: CharacterBody3D = $"../.."
 @onready var movement: PlayerMovement3D = %Movement
 @onready var sprint: Node = %Sprint
 @onready var jump: PlayerJump3D = %Jump
+@onready var crouch: PlayerCrouch3D = %Crouch
+#endregion
+
 
 func _input(event) -> void:
 	#region MOVEMENT
@@ -27,4 +33,17 @@ func _input(event) -> void:
 		sprint.start()
 	elif Input.is_action_just_released("sprint"):
 		sprint.stop()
+	#endregion
+
+
+	#region CROUCH
+	if Input.is_action_pressed('debug_crouch_up'):
+		crouch.up()
+	elif Input.is_action_pressed('debug_crouch_down'):
+		crouch.down()
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_WHEEL_UP:
+			crouch.up()
+		elif event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
+			crouch.down()
 	#endregion
