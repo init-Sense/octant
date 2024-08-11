e5801627b5e0b24af471c1742e6c40e42f83eaf0
bottolo <samuele.castiglia.98@gmail.com>
2024-08-11 11:57:46 +0200

    style: Better regions for head bob

--- scripts/player/head_bob.gd
+++ scripts/player/head_bob.gd
@@ -9,38 +9,50 @@ class_name PlayerHeadBob
 #endregion
 
 
-#region VARIABLES
+#region CONSTANTS
+#region HEAD BOB CONSTANTS
 const DEFAULT_AMPLITUDE: float = 0.05
 const DEFAULT_FREQUENCY: float = 5.0
 const AMPLITUDE_FACTOR: float = 0.15
 const FREQUENCY_FACTOR: float = 0.4
 const RESET_SPEED: float = 1.0
 const CAMERA_HEIGHT: float = 0.5
+const X_FACTOR: float = 0.6
+const NOISE_STRENGTH: float = 0.05
+#endregion
+
+#region LANDING CONSTANTS
 const BASE_LANDING_IMPACT_AMPLITUDE: float = 0.05
 const MAX_LANDING_IMPACT_AMPLITUDE: float = 0.15
 const LANDING_IMPACT_DURATION: float = 0.2
 const LANDING_RESET_SPEED: float = 2.0
 const MAX_IMPACT_HEIGHT: float = 10.0
-const NOISE_STRENGTH: float = 0.05
-const X_FACTOR: float = 0.6
+#endregion
+#endregion
 
 
+#region VARIABLES
+#region HEAD BOB VARIABLES
 var bob_time: float = 0.0
 var current_offset: Vector3 = Vector3.ZERO
 var current_amplitude: float = DEFAULT_AMPLITUDE
 var current_frequency: float = DEFAULT_FREQUENCY
 var is_bobbing: bool = false
+var noise: FastNoiseLite = FastNoiseLite.new()
+#endregion
+
+#region LANDING VARIABLES
 var landing_impact_time: float = 0.0
 var is_landing: bool = false
 var landing_offset: float = 0.0
 var max_jump_height: float = 0.0
 var jump_start_height: float = 0.0
 var current_landing_impact_amplitude: float = BASE_LANDING_IMPACT_AMPLITUDE
-var noise: FastNoiseLite = FastNoiseLite.new()
+#endregion
 #endregion
 
 
-#region LIFECYCLE
+#region LIFECYCLE METHODS
 func _ready() -> void:
 	jump.connect("jumped", Callable(self, "on_player_jumped"))
 	jump.connect("landed", Callable(self, "on_player_landed"))
@@ -48,24 +60,20 @@ func _ready() -> void:
 
 
 func _physics_process(delta: float) -> void:
+	handle_head_bob(delta)
+	handle_landing(delta)
+	update_camera_position()
+#endregion
+
+
+#region HEAD BOB METHODS
+func handle_head_bob(delta: float) -> void:
 	if player.is_moving() and not player.is_jumping():
 		apply_head_bob(delta)
 	elif is_bobbing:
 		smooth_reset_head_bob(delta)
-	
-	if player.is_jumping():
-		track_jump_height()
-	
-	if is_landing:
-		apply_landing_impact(delta)
-	elif landing_offset != 0:
-		smooth_reset_landing_impact(delta)
-	
-	camera.position = Vector3(current_offset.x, CAMERA_HEIGHT + current_offset.y + landing_offset, current_offset.z)
-#endregion
 
 
-#region HEAD BOB
 func apply_head_bob(delta: float) -> void:
 	handle_bob_wave()
 	bob_time += current_frequency * delta
@@ -93,7 +101,17 @@ func handle_bob_wave() -> void:
 #endregion
 
 
-#region LANDING
+#region LANDING METHODS
+func handle_landing(delta: float) -> void:
+	if player.is_jumping():
+		track_jump_height()
+	
+	if is_landing:
+		apply_landing_impact(delta)
+	elif landing_offset != 0:
+		smooth_reset_landing_impact(delta)
+
+
 func on_player_jumped() -> void:
 	jump_start_height = player.global_position.y
 	max_jump_height = jump_start_height
@@ -122,4 +140,10 @@ func apply_landing_impact(delta: float) -> void:
 
 func smooth_reset_landing_impact(delta: float) -> void:
 	landing_offset = move_toward(landing_offset, 0, LANDING_RESET_SPEED * delta)
+#endregion
+
+
+#region CAMERA UPDATE
+func update_camera_position() -> void:
+	camera.position = Vector3(current_offset.x, CAMERA_HEIGHT + current_offset.y + landing_offset, current_offset.z)
 #endregion
