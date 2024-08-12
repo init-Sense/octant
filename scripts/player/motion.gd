9e8a7d4955be01e4e3f1f48f0772ae2427932ad9
bottolo <samuele.castiglia.98@gmail.com>
2024-08-12 15:04:11 +0200

    fix: Walk delay now works properly

--- scripts/player/motion.gd
+++ scripts/player/motion.gd
@@ -18,6 +18,7 @@ const DECELERATION: float = 15.0
 const MAX_FOV_CHANGE: float = 12.0
 const FOV_TRANSITION_SPEED: float = 50.0
 const FOV_RESET_THRESHOLD: float = 0.1
+const WALK_DELAY: float = 0.1
 #endregion
 
 
@@ -25,6 +26,8 @@ const FOV_RESET_THRESHOLD: float = 0.1
 var target_speed: float = 0.0
 var current_fov_change: float = 0.0
 var is_sprinting: bool = false
+var walk_timer: float = 0.0
+var is_walk_delayed: bool = false
 #endregion
 
 
@@ -32,6 +35,7 @@ var is_sprinting: bool = false
 func _physics_process(delta: float) -> void:
 	direction.current_speed = move_toward(direction.current_speed, target_speed, DECELERATION * delta)
 	update_fov(delta)
+	update_walk_timer(delta)
 #endregion
 
 
@@ -39,21 +43,29 @@ func _physics_process(delta: float) -> void:
 func idle() -> void:
 	player.set_idle()
 	target_speed = 0.0
+	is_walk_delayed = false
 
 
 func walk() -> void:
-	player.set_walking()
-	target_speed = WALKING_SPEED
+	if not is_walk_delayed:
+		is_walk_delayed = true
+		walk_timer = 0.0
+	elif walk_timer >= WALK_DELAY:
+		player.set_walking()
+		target_speed = WALKING_SPEED
+		is_walk_delayed = false
 
 
 func run() -> void:
 	player.set_running()
 	target_speed = SPRINT_SPEED
+	is_walk_delayed = false
 
 
 func sneak() -> void:
 	player.set_sneaking()
 	update_sneaking_speed()
+	is_walk_delayed = false
 
 
 func update_sneaking_speed() -> void:
@@ -77,12 +89,20 @@ func update_movement_state() -> void:
 
 func start_sprint() -> void:
 	is_sprinting = true
+	is_walk_delayed = false
 	update_movement_state()
 
 
 func stop_sprint() -> void:
 	is_sprinting = false
 	update_movement_state()
+
+
+func update_walk_timer(delta: float) -> void:
+	if is_walk_delayed:
+		walk_timer += delta
+		if walk_timer >= WALK_DELAY:
+			update_movement_state()
 #endregion
 
 
