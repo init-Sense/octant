84f352a48c46d9187fc903bf954ea972e7ec6e33
bottolo <samuele.castiglia.98@gmail.com>
2024-10-01 21:56:55 +0200

    feat: Added music!

--- scripts/scenes/current_scene.gd
+++ scripts/scenes/current_scene.gd
@@ -1,8 +1,11 @@
 extends Node
 
 @export var scene_name: String
+@export var soundtrack: String
 
 func _ready():
+	if soundtrack:
+		MusicManager.play_varied(scene_name, soundtrack, -15.0)
 	if scene_name:
 		SceneManager.current_scene = scene_name
 		print("Current scene set to: ", scene_name)
